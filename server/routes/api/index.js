c0d2cf02cbd863c668288d7074092cae3e697714
Mostafa Mohmed Hisham Ahmed Mostafa <mmham3919@gmail.com>
2020-09-11 20:20:33 -0700

    fix import of auth route

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const apiRouters = require('express').Router();
-const authRouter = require('./auth.router');
+const authRouter = require('./auth.route');
 
 // Routes
 router.use('/api', apiRouters);
