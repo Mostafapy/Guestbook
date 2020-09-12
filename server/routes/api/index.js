451954b809d0a397b786a168307b65d9c099ed85
Mostafa Mohmed Hisham Ahmed Mostafa <mmham3919@gmail.com>
2020-09-11 20:44:00 -0700

    adding message route to index route file

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 const apiRouters = require('express').Router();
 const authRouter = require('./auth.route');
+const messageRoute = require('./message.route');
 
 // Routes
 router.use('/api', apiRouters);
 apiRouters.use('/auth', authRouter);
+apiRouters.use('/message', messageRoute);
 
 module.exports = router;
