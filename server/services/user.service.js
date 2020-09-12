a827ae415248cbaecc30372872ef337564ec886e
Mostafa Mohmed Hisham Ahmed Mostafa <mmham3919@gmail.com>
2020-09-11 17:46:17 -0700

    add retrieveUserById service user

--- server/services/user.service.js
+++ server/services/user.service.js
@@ -1,9 +1,34 @@
 const UserModel = require('../models/user.model');
 
-const retrieveUser = (email) => new Promise((resolve, reject) => {
+/**
+ *  Function to retrieve the requested user by the email
+ * @param {String} email
+ * @returns {Promise | Error}
+ */
+const retrieveUserByEmail = (email) => new Promise((resolve, reject) => {
     UserModel.findOne({
         email,
       }).then(user => resolve(user)).catch(err => reject(err))
 });
 
-module.exports = { retrieveUser };
+/**
+ *  Function to retrieve the requested user by the id
+ * @param {String} id
+ * @returns {Promise | Error}
+ */
+
+const retrieveUserById = async id => {
+  try {
+    const user = await userModel.findById(id).select('-password');
+
+    return Promise.resolve(user);
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      return Promise.reject(new Error(`No Found Template`));
+    }
+
+    return Promise.reject(err);
+  }
+}
+
+module.exports = { retrieveUserByEmail, retrieveUserById };
