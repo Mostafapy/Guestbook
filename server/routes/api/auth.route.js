3a604d2f245c9e57783f61c4bab8cd0788513e3b
Mostafa Mohmed Hisham Ahmed Mostafa <mmham3919@gmail.com>
2020-09-11 20:20:13 -0700

    rename auth route

--- server/routes/api/auth.route.js
+++ server/routes/api/auth.route.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 
 /** Controllers */
-const { userRegistration, login } = require('./../../controllers/auth.controller');
+const { userRegistration, login } = require('../../controllers/auth.controller');
 
 const router = express.Router();
 
