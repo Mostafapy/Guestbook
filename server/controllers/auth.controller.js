9d109dfbf879eb8a87665f433e8417df46cbd4d1
Mostafa Mohmed Hisham Ahmed Mostafa <mmham3919@gmail.com>
2020-09-11 17:09:41 -0700

    add login controller

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,7 +1,13 @@
 /* Models */
 const UserModel = require('./../models/user.model');
 
+/* Services */
+const retrieveUserService = require('./../services/retrieveUser.service');
+
 const logger = require('./../utils/logger')('Controllers:AuthController');
+const bcrypt = require('bcrypt');
+const { promisify } = require('util');
+const jwt = require('jsonwebtoken');
 
 // @desc Register User
 // @route POST /api/auth/register
@@ -10,11 +16,9 @@ const userRegistration = async (req, res) => {
     const { name, email, password } = req.body;
 
     try {
-      const existedUser = await UserModel.findOne({
-        email,
-      })
+      const retrievedUser = await retrieveUserService(email);
 
-      if (existedUser) {
+      if (retrievedUser) {
         return res.status(400).json({
           success: false,
           msg: 'Invalid Credentials user already exists',
@@ -36,7 +40,7 @@ const userRegistration = async (req, res) => {
      });
 
     } catch (err) {
-      logger.error('userRegistration()', err.message);
+      logger.error('userRegistration', err.message);
   
       return res.status(500).json({
         success: false,
@@ -46,4 +50,61 @@ const userRegistration = async (req, res) => {
     }
 };
 
-module.exports = { userRegistration }
+// @desc Login
+// @route POST /api/auth/login
+// @access Private
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    
+    const retrievedUser = await retrieveUserService(email);
+
+    if (!retrievedUser) {
+        return res.status(400).json({
+          success: false,
+          msg: 'Invalid Credentials user already exists',
+          data: null,
+        });
+    }
+
+    // check is password
+    const isMatch = await bcrypt.compare(password, retrievedUser.password);
+    
+    if (!isMatch) {
+      return res.status(400).json({
+        success: false,
+        msg: 'Invalid Credentials password is not matching',
+        data: null,
+      });
+    }
+
+    const payload = {
+      user: {
+        id: retrievedUser.id,
+      },
+    };
+
+    const jwtSign = promisify(jwt.sign);
+
+    const token = await jwtSign(payload, process.env.JWT_SECRET, {
+      expiresIn: process.env.JWT_EXPIRE,
+    });
+
+    return res.status(201).json({
+      success: true,
+      msg: 'Successfully sign in',
+      data: { token },
+   });
+
+  } catch (err) {
+    logger.error('login', err.message);
+  
+    return res.status(500).json({
+      success: false,
+      msg: 'Server Error',
+      data: null,
+   });
+  }
+}
+
+module.exports = { userRegistration, login }
