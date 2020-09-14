973eac2c734cdd821608fe5e2f753cc5b5c30cba
Mostafa Mohmed Hisham Ahmed Mostafa <mmham3919@gmail.com>
2020-09-14 02:45:44 -0700

    index of reducer

---
+++ client/src/reducers/index.js
@@ -0,0 +1,22 @@
+  
+import thunk from 'redux-thunk';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+
+import { messageReducer, selectedMessageReducer } from './messege-reducer';
+import { authReducer } from './auth-reducers';
+import { reducer as formReducer } from 'redux-form';
+
+export const init = () => {
+  const reducer = combineReducers({
+    messages: messageReducer,
+    message: selectedMessageReducer,
+    form: formReducer,
+    auth: authReducer,
+  });
+
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+  const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
+
+  return store;
+}
